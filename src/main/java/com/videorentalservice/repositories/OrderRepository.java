2e6bd11f2c082d2ff6abba76e893c94d530ab6f5
Ravereign <matus.turic@gmail.com>
2017-03-25 11:52:52 +0100

    Fixed query Order

--- src/main/java/com/videorentalservice/repositories/OrderRepository.java
+++ src/main/java/com/videorentalservice/repositories/OrderRepository.java
@@ -4,6 +4,7 @@ import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.core.types.dsl.StringPath;
 import com.videorentalservice.models.Order;
 import com.videorentalservice.models.QBooking;
+import com.videorentalservice.models.QOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -15,10 +16,10 @@ import org.springframework.data.querydsl.binding.SingleValueBinding;
  */
 public interface OrderRepository extends JpaRepository<Order, Integer>,
         QueryDslPredicateExecutor<Order>,
-        QuerydslBinderCustomizer<QBooking> {
+        QuerydslBinderCustomizer<QOrder> {
 
     @Override
-    default public void customize(QuerydslBindings bindings, QBooking root) {
+    default public void customize(QuerydslBindings bindings, QOrder root) {
         bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
     }
 }
