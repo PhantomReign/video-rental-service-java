085ef313d1deb0686793940507554146cdbb29b4
Ladislav Šulák <mikerusty@azet.sk>
2017-03-28 03:20:33 +0200

    + Validators for first name, last name and phone number.

--- src/main/java/com/videorentalservice/validators/UserValidator.java
+++ src/main/java/com/videorentalservice/validators/UserValidator.java
@@ -34,6 +34,11 @@ public class UserValidator implements Validator {
         User userByEmail = userService.getByEmail(email);
         User userByUserName = userService.getByUserName(userName);
 
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+
+        String phone = user.getPhone();
+
         if(userByEmail != null){
             errors.rejectValue("email",
                     "error.exists",
@@ -48,7 +53,35 @@ public class UserValidator implements Validator {
                     "Používateľské meno "+userName+" sa už používa");
         }
 
+        if(! firstName.isEmpty()) {
+            if (!firstName.toLowerCase().matches("[a-záéíĺóŕúýčďľňšťžäôůřěöüőű]+$")) {
+                errors.rejectValue("firstName",
+                        "error.invalid",
+                        new Object[]{firstName},
+                        "Meno " + firstName + " obsahuje nepovolené znaky");
+            }
+        }
 
+        if(!lastName.isEmpty()) {
+            if (!lastName.toLowerCase().matches("[a-záéíĺóŕúýčďľňšťžäôůřěöüőű]+$")) {
+                errors.rejectValue("lastName",
+                        "error.invalid",
+                        new Object[]{lastName},
+                        "Priezvisko "+lastName+" obsahuje nepovolené znaky");
+            }
+        }
+
+        // Regexp on phone number:
+        // Optional call prefix, it starts with '+' and can have 2 or 3 digits (Austria has +43) or '0' instead of it.
+        // Then the number - 9 digits and they can be separated by space always after a group of 3 digits.
+        if(!phone.isEmpty()) {
+            if (!phone.matches("^((\\+[0-9]{2,3})? ?|[0]?)[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}$")) {
+                errors.rejectValue("phone",
+                        "error.invalid",
+                        new Object[]{phone},
+                        "Telefónne číslo " + phone + " nie je v správnom formáte");
+            }
+        }
     }
 
 }
