9bfd8a82da26e3394b4d3fdbe2553ddd44994900
Ladislav Šulák <mikerusty@azet.sk>
2017-03-29 10:58:07 +0200

    + Validator for email in user registration/adding.

--- src/main/java/com/videorentalservice/validators/UserValidator.java
+++ src/main/java/com/videorentalservice/validators/UserValidator.java
@@ -82,6 +82,12 @@ public class UserValidator implements Validator {
                         "Telefónne číslo " + phone + " nie je v správnom formáte");
             }
         }
-    }
 
+        if(!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,6}$")) {
+            errors.rejectValue("email",
+                    "error.invalid",
+                    new Object[]{email},
+                    "Email " + email + " nie je v správnom formáte");
+        }
+    }
 }
